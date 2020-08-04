0c80fdc54cb9db8bc0be0482c375f7c67c908407
liushiyu8908908 <619461946@qq.com>
2020-08-04 23:33:53 +0800

    1

---
+++ src/Test1.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @ClassName Test
+ * @Description TODO
+ * @Author 18229870721
+ * @Date 2020/8/4 23:24
+ **/
+public class Test1 {
+ @Test
+ public void a(){
+   
+ }
+}
